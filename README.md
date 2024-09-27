0c9c8b436db3094c60716f676957e6df9d1a1869
Shawn Tabrizi <shawntabrizi@gmail.com>
2024-09-27 13:12:30 -0400

    template: main function

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
-# Solution
+# TODO
 
-You can find the solution to the previous step here.
-
-<!-- Generally, this page is not shown to the user. -->
+- Explain RPC endpoints
+- Perhaps hint at light clients
+- Perhaps touch on async functions, even though we dont use async yet.
+- Note in the output of `console.log({ polkadotClient })`:
+   - The `getChainSpecData` method
+   - The `getFinalizedBlock` method
+   - These will be used in the next step
--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+// TODO: Import `process` from `node:process`.
 import { getWsProvider } from "polkadot-api/ws-provider/web";
 import { createClient, type PolkadotClient } from "polkadot-api";
 
@@ -7,3 +8,13 @@ function makeClient(endpoint: string): PolkadotClient {
 	const client = createClient(provider);
 	return client;
 }
+
+// TODO:
+// - Create a new `async` function called `main`.
+//   - Call `makeClient` with the endpoint `"wss://rpc.polkadot.io"`.
+//   - Assign the result to a new constant `polkadotClient`.
+//   - Print the `polkadotClient` object with `console.log` to take a peek inside.
+//   - Use `console.log` to print a "Done!" message to show you app completed successfully.
+//   - Exit the process by calling `process.exit(0)`.
+
+// TODO: Call your `main()` function.
